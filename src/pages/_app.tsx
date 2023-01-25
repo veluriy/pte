9a513d6ea4c8d4ffe57c325bc2680cfef0e51bd5
ptlx <ptlx@outlook.jp>
2023-01-25 23:42:28 +0900

    fix errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New&display=swap" rel="stylesheet"/>
             </Head>
             <ChakraProvider theme={theme}>
